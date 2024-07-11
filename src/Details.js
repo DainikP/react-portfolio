a7461141b7cec77af80115ca679b9cb61dc6dc64
dainik <dainik.prajapati@smartters.in>
2024-07-11 22:35:08 +0530

    react portfolio

---
+++ src/Details.js
@@ -0,0 +1,164 @@
+
+
+// Profile Image
+import profile from "./assets/ui/profile.jpg";
+// Tech stack images
+import html from "./assets/techstack/html.png";
+import css from "./assets/techstack/css.png";
+import sass from "./assets/techstack/sass.png";
+import js from "./assets/techstack/js.png";
+import react from "./assets/techstack/react.png";
+import redux from "./assets/techstack/redux.png";
+import tailwind from "./assets/techstack/tailwind.png";
+import vscode from "./assets/techstack/vscode.png";
+import github from "./assets/techstack/github.png";
+import git from "./assets/techstack/git.png";
+import npm from "./assets/techstack/npm.png";
+import postman from "./assets/techstack/postman.png";
+
+import Nextjs from "./assets/techstack/nextjs.png";
+import featherjs from "./assets/techstack/featherjs.png";
+import expressjs from "./assets/techstack/expressjs.png";
+import mongodb from "./assets/techstack/mongodb.png";
+import Mysql from "./assets/techstack/Mysql.png";
+import Yarn from "./assets/techstack/yarn.png";
+
+// Porject Images
+import projectImage1 from "./assets/projects/project1.jpg";
+import projectImage2 from "./assets/projects/project2.jpg";
+import projectImage3 from "./assets/projects/project3.jpg";
+import projectImage4 from "./assets/projects/project4.jpg";
+import projectImage5 from "./assets/projects/project5.jpg";
+import projectImage6 from "./assets/projects/project6.jpg";
+
+
+
+// Enter your Personal Details here
+export const personalDetails = {
+  name: "Dainik Prajapati",
+  tagline: "Mern Stack Webdeveloper",
+  img: profile,
+  about: `MERN Stack Developer with a B.E. in Electronics
+passionate about building awesome web apps.
+Thrives in collaborative environments and loves
+tackling challenges to create user-friendly and
+scalable solutions. Looking for a dynamic team to
+push the boundaries of web development!`,
+};
+
+// Enter your Social Media URLs here
+export const socialMediaUrl = {
+  linkdein: "https://www.linkedin.com/in/dainikp0603/",
+  github: "https://www.github.com/DainikP",
+  twitter: " https://x.com/dainik_633/",
+  instagram: "https://www.instagram.com/_dainik_633/",
+ };
+
+// Enter your Work Experience here
+export const workDetails = [
+  {
+    Position: "Internship",
+    Company: `Smartters`,
+    Location: "Ahmedabad",
+    Type: "Internship",
+    Duration: "Feb 2024 - Jun 2024",
+  },
+ 
+];
+
+// Enter your Education Details here
+export const eduDetails = [
+  {
+    Position: "B.Tech in Electronics",
+    Company: `Birla Vishvakarma Mahavidyalaya`,
+    Location: "Vvnagar Gujarat",
+    Type: "Full Time",
+    Duration: "Aug 2020 - Apr 2024",
+  },
+];
+
+// Tech Stack and Tools
+export const techStackDetails = {
+  html: html,
+  css: css,
+  js: js,
+  react: react,
+  redux: redux,
+  sass: sass,
+  tailwind: tailwind,
+  vscode: vscode,
+  postman: postman,
+  npm: npm,
+  git: git,
+  github: github,
+  nextJs:Nextjs,
+  featherjs:featherjs,
+  expressjs:expressjs,
+  mongodb:mongodb,
+  Mysql:Mysql,
+  Yarn:Yarn
+};
+
+// Enter your Project Details here
+export const projectDetails = [
+  {
+    title: "Portfolio Website",
+    image: projectImage1,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "Reactjs, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 2",
+    image: projectImage2,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 3",
+    image: projectImage3,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 4",
+    image: projectImage4,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 5",
+    image: projectImage5,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+  {
+    title: "Project title 6",
+    image: projectImage6,
+    description: `This is sample project description random things are here in description This is sample
+project lorem ipsum generator for dummy content`,
+    techstack: "HTML/CSS, JavaScript",
+    previewLink: "https://google.com",
+    githubLink: "https://github.com",
+  },
+];
+
+// Enter your Contact Details here
+export const contactDetails = {
+  email: "dainikjp0603@gmail.com",
+  phone: "+91 9429061596",
+};
